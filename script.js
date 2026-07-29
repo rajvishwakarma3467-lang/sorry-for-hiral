104faa2361fb42eaca7144c876e60ae698373a60
rajvishwakarma3467-lang <rajvishwakarma3467@gmail.com>
2026-07-30 03:32:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,73 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-    "❤️ Thank you for giving me one chance. I promise I'll never make you feel alone again. ❤️";
+function openLetter(){
+
+    document.querySelector(".welcome").style.display="none";
+
+    document.querySelector(".letter").style.display="block";
+
+    typeWriter();
+
+}
+
+
+const text = 
+"Hirruuuuu... 🥺 I know I made a mistake. I just want you to know that you are very special to me. ❤️";
+
+
+let index = 0;
+
+
+function typeWriter(){
+
+    if(index < text.length){
+
+        document.getElementById("typing").innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(typeWriter,50);
+
+    }
+
+}
+
+
+
+function forgive(){
+
+    document.getElementById("final").innerHTML =
+    "🥹❤️ Thank you for reading my message. I truly hope you can forgive me. You will always be special to me. ❤️<br><br>— Raj";
+
+    createHearts();
+
+}
+
+
+
+function createHearts(){
+
+    for(let i=0;i<30;i++){
+
+        let heart=document.createElement("div");
+
+        heart.innerHTML="❤️";
+
+        heart.style.position="fixed";
+        heart.style.left=Math.random()*100+"%";
+        heart.style.top="100%";
+        heart.style.fontSize="25px";
+
+        heart.style.animation=
+        "float 4s linear";
+
+        document.body.appendChild(heart);
+
+
+        setTimeout(()=>{
+
+            heart.remove();
+
+        },4000);
+
+    }
+
 }
