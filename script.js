ebcb97724ac0c9db029b7809a68e8492e6380202
rajvishwakarma3467-lang <rajvishwakarma3467@gmail.com>
2026-07-30 19:49:18 +0530

    Add stars animation to script.js

--- script.js
+++ script.js
@@ -330,3 +330,28 @@ heart.remove();
 
 
 },800);
+// ==========================
+// STARS
+// ==========================
+
+const stars=document.getElementById("stars");
+
+for(let i=0;i<120;i++){
+
+const star=document.createElement("div");
+
+star.className="star";
+
+star.style.left=Math.random()*100+"vw";
+
+star.style.top=Math.random()*100+"vh";
+
+star.style.animationDelay=
+(Math.random()*2)+"s";
+
+star.style.animationDuration=
+(1+Math.random()*3)+"s";
+
+stars.appendChild(star);
+
+}
